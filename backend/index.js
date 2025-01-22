9a132a95694ac8eb8ec6c9ac600e80748568770f
amanpreet singh <amanpreetsingh2524@gmail.com>
2025-01-22 19:48:31 +0530

    deployment config

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import emailRoutes from "./routes/email.route.js";
 
@@ -10,11 +11,20 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/emails", emailRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
